535befec1b7d666a29aa8931527f9ffb3c270bea
IEUser <IEUser>
2016-06-06 14:45:19 -0700

    created version task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,18 @@
 const gulp = require('gulp');
+const semver = require('semver');
+
+gulp.task('version', function(done) {
+    const packageJson = require('./package.json');
+    const expectedVersion = packageJson.engines.node;
+    const actualVersion = process.version;
+    console.log('Checking node version: ');
+    if (semver.gt(expectedVersion,actualVersion)){
+        console.log('Incorrect node version. Expected ' + expectedVersion +
+        '. Actual: ' + actualVersion);
+        process.exit(1);
+    }
+    done();
+});
 
 //default task
 gulp.task('default', function(done) {
