b4104546972543ac26d37daa9d511fddad531d00
IEUser <IEUser>
2016-06-07 08:35:20 -0700

    installed and configured jshint and gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 const gulp = require('gulp');
+const jshint = require('gulp-jshint');
 const semver = require('semver');
+
+gulp.task('jshint', function() {
+    console.log('Linting JavaScript: ');
+    return gulp.src(['gulpfile.js', 'src/**/*.js'])
+        .pipe(jshint({
+            esversion: 6,
+            node: true,
+            browser: true,
+            globals: []
+        }))
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'));
+});
 
 gulp.task('version', function(done) {
     const packageJson = require('./package.json');
@@ -15,7 +29,8 @@ gulp.task('version', function(done) {
 });
 
 //default task
-gulp.task('default', gulp.series('version', function(done) {
+gulp.task('default', gulp.series(gulp.parallel('version', 'jshint'),
+    function(done){
     console.log('BUILD OK');
     done();
 }));
