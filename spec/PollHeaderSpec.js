6fdfcf2577fd18faff07d3e735b685740639c7bd
IEUser <IEUser>
2016-06-08 10:13:07 -0700

    adding specs

---
+++ spec/PollHeaderSpec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import PollHeader from '../src/components/PollHeader.js';
+
+describe('Poll Header', function() {
+    it('renders without problems', function() {
+        var pollheader = TestUtils.renderIntoDocument(<PollHeader />);
+        expect(pollheader).toEqual(jasmine.anything());
+    });
+});
