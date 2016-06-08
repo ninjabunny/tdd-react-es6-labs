e35dd8f7f46aa904d820b1800d26cbbcf4f7b260
IEUser <IEUser>
2016-06-08 08:31:53 -0700

    installed react and created first react component

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,7 @@
-import * as sayHello from './sayHello.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-window.addEventListener('load', function(){
-    document.getElementById('welcome-message').innerHTML = sayHello.greet('Chris');
-});
+ReactDOM.render(
+    <h1>Hello, World!</h1>,
+    document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,15 @@ module.exports = {
             test : /.js$/,
             loader : 'babel-loader',
             query : {
-                presets: ['es2015']
+                presets: ['es2015','react']
             }
         }]
-    }
+    }/*,
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': '"production"'
+            }
+        })
+    ]*/
 };
